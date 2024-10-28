58dbcb75fc9856ae4549e66654d0b3cbb9a675c0
MrClock <jz.zalan@gmail.com>
2024-10-28 22:37:20 +0100

    Create HandleClass.m

---
+++ Definíciók/HandleClass.m
@@ -0,0 +1,16 @@
+classdef HandleClass < handle
+    %HANDLECLASS Example value class.
+    properties
+        prop (1,1) double % Internal property
+    end
+    
+    methods
+        function obj = HandleClass(val)
+            %HANDLECLASS Initialize new instance with given value.
+            arguments
+                val (1,1) double
+            end
+            obj.prop = val;
+        end
+    end
+end
